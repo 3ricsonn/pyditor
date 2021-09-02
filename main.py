54acd4c675d7cbc45dbca2915f019b5b4683d518
3ricsonn <sebastian@bensch-erpel.de>
2021-09-02 17:08:20 +0200

    Add files via upload
    Layout draft

--- main.py
+++ main.py
@@ -1,10 +1,67 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-import fitz
+# from PIL import Image, ImageTk
+# import fitz
+
+from widgets import ScrollFrame
 
 
-class PageViewer(tk.Frame):
-    pass
+class MultiplePageViewer(ScrollFrame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self.pages = []
+        self.selection = []
+
+    def load_pages(self, document: fitz.Document) -> None:
+        # clear viewPort frame
+        self.clear()
+
+        for i, page in enumerate(document, start=1):
+            pix = page.get_pixmap()
+
+            # set the mode depending on alpha
+            mode = "RGBA" if pix.alpha else "RGB"
+            img = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
+
+            # rescale image to fit in the frame
+            scale = (self.viewPort.winfo_width() - 16) / img.size[0]
+            assert scale > 0
+            scaleImg = img.resize((int(img.size[0] * scale), int(img.size[1] * scale)))
+
+            # convert to a displayable tk-image
+            tkImg = ImageTk.PhotoImage(scaleImg)
+
+            labelImg = tk.Label(master=self.viewPort, text=f"Page {i}", image=tkImg, compound="top", padx=3)
+            labelImg.image = tkImg
+            labelImg.pack(pady=5, padx=5)
+            
+            # bind an event whenever a page is clicked to select it
+            labelImg.bind("<Button-1>", func=self.add_page_to_selection)
+            labelImg.bind("<Control-Button-1>", func=self.select_multiple_pages)
+
+            # append label to pages to later display whether its selected
+            self.pages.append(labelImg)
+
+    def clear(self) -> None:
+        for widget in self.viewPort.winfo_children():
+            widget.destroy()
+
+    def clear_selection(self) -> None:
+        for widget in self.viewPort.winfo_children():
+            widget.config(bg="#cecfd0")
+
+        self.selection.clear()
+
+    def select_page(self, event:tk.Event) -> None:
+        self.clear_selection()
+
+        event.widget.config(bg="blue")
+        self.selection.append(event.widget["text"])
+
+    def select_multiple_pages(self, event:tk.Event) -> None:
+        event.widget.config(bg="blue")
+        self.selection.append(event.widget["text"])
 
 
 class PyditorApplication(tk.Frame):
@@ -15,57 +72,81 @@ class PyditorApplication(tk.Frame):
         self.parent = parent
 
         # == divide window in panels ==
-        self.toolbarPanel = tk.PanedWindow(master=self, orient=tk.HORIZONTAL)
+        # -- create toolbar panel--
+        self.toolbarPanel = tk.PanedWindow(master=self, orient=tk.VERTICAL)
         self.toolbarPanel.pack(expand=True, fill="both")
-        self.sash1_x = 180
-        self.sash2_x = 1050
+        
+        # -- create toolbar frame
+        self.toolbarFrame = tk.Frame(master=self.toolbarPanel, bg="red")
+        self.toolbarPanel.add(self.toolbarFrame)
+        
+        # -- create page-view-bar panel --
+        self.pageViewerPanel = tk.PanedWindow(master=self, orient=tk.HORIZONTAL)
+        self.toolbarPanel.add(self.pageViewerPanel)
 
+        # create placeholder document
         self.PDFDocument: fitz.Document = fitz.Document()
 
-    def load_components_view(self):
+    def load_components_view(self) -> None:
         self.clear_frame()
+        
+        toolbar = tk.Label(master=self.toolbarFrame, text="top_toolbar", bg="red")
+        toolbar.pack(pady=10)
 
-        left = tk.Label(master=self.toolbarPanel, text="left_page_viewer", bg="red")
-        self.toolbarPanel.add(left)
+        self.leftPageViewer = MultiplePageViewer(parent=self.pageViewbarPanel)
+        self.pageViewbarPanel.add(self.leftPageViewer)
 
-        editor = tk.Label(master=self.toolbarPanel, text="single_page_editor", bg="blue")
-        self.toolbarPanel.add(editor)
+        editor = tk.Label(master=self.pageViewbarPanel, text="single_page_editor", bg="blue")
+        self.pageViewbarPanel.add(editor)
 
-        right = tk.Label(master=self.toolbarPanel, text="tools?", bg="green")
-        self.toolbarPanel.add(right)
-
-        self.toolbarPanel.update()
-        self.toolbarPanel.sash_place(0, 180, 1)
-        self.toolbarPanel.sash_place(1, 1050, 1)
+        self.pageViewbarPanel.update()
+        self.pageViewbarPanel.sash_place(0, 180, 1)
+        
+        # display already loaded document
+        if self.PDFDocument:
+            self.leftPageView.load_pages(self.PDFDocument)
+        
 
     def load_components_edit(self):
         self.clear_frame()
 
-        left = tk.Label(master=self.toolbarPanel, text="left_page_viewer", bg="red")
-        self.toolbarPanel.add(left)
+        toolbar = tk.Label(master=self.toolbarFrame, text="top_toolbar", bg="red")
+        toolbar.pack(pady=10)
+        
+        left = tk.Label(master=self.pageViewbarPanel, text="selected_page_view", bg="green")
+        self.pageViewbarPanel.add(left)
 
-        editor = tk.Label(master=self.toolbarPanel, text="page_arrangement_editor", bg="blue")
-        self.toolbarPanel.add(editor)
+        editor = tk.Label(master=self.pageViewbarPanel, text="page_arrangement_editor", bg="blue")
+        self.pageViewbarPanel.add(editor)
 
-        right = tk.Label(master=self.toolbarPanel, text="tools?", bg="green")
-        self.toolbarPanel.add(right)
-
-        self.toolbarPanel.update()
-        self.toolbarPanel.sash_place(0, 180, 1)
-        self.toolbarPanel.sash_place(1, 1050, 1)
+        self.pageViewbarPanel.update()
+        self.pageViewbarPanel.sash_place(0, 180, 1)
 
     def clear_frame(self):
-        for widget in self.toolbarPanel.winfo_children():
+        for widget in self.pageViewbarPanel.winfo_children():
             widget.destroy()
 
     def open_file(self):
         """opens a filedialog to open the to editing pdf and convert it to a 'fitz.Document'"""
         pdf_file = askopenfilename(title="Choose your PDF you want to edit:", filetypes=[("PDF-Files", "*.pdf")])
-
+        
         if pdf_file:
-            self.PDFDocument = fitz.open(pdf_file)
+            self.PDFDocument = fitz.Document(pdf_file)
 
+            # rename title with according file path
             self.parent.title("Pyditor - editing: " + pdf_file)
+
+            # display file in page viewer
+            self.leftPageViewer.clear()
+            self.leftPageViewer.load_pages(self.PDFDocument)
+
+    def save_file(self):
+        """saves the edited pdf file using the metadata title as name"""
+        pass
+
+    def save_file_name(self):
+        """saves the edited pdf-file asking for a name"""
+        pass
 
 
 def print_sash_pos():
@@ -76,7 +157,7 @@ def print_sash_pos():
 if __name__ == "__main__":
     # create the window and do basic configuration
     rootWindow = tk.Tk()
-    rootWindow.title("Pyditor - edit PDFs")
+    rootWindow.title("Pyditor - edit PDFs")  # TODO: more meaningful name
     rootWindow.geometry("1200x1300")
 
     # creating and packing the Main Application
@@ -94,6 +175,8 @@ if __name__ == "__main__":
     fileMenu = tk.Menu(master=mainMenu)
     mainMenu.add_cascade(label="File", menu=fileMenu)
     fileMenu.add_command(label="Open", command=app.open_file)
+    fileMenu.add_command(label="Save", command=app.save_file)
+    fileMenu.add_command(label="Save as...", command=app.save_file_name)
     fileMenu.add_separator()
     fileMenu.add_command(label="Exit", command=rootWindow.quit)
 
